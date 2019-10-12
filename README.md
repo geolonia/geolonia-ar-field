152548ebca9444f961086df4c37a36f7e4b875c1
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-12 10:36:02 +0900

    Add dev env

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Geolonia AR Field
 
 This repository hosts an experimental `style.json` for Mapbox GL JS.
+
+## Development
+
+```shell
+$ git clone git@github.com:geolonia/geolonia-ar-field.git
+$ cd geolonia-ar-field
+$ yarn # or npm install
+$ npm start
+```
+
+## build
+
+```shell
+$ npm run build
+```
+
+Be sure to run `npm run build` before pushing it to the `master` branch.
---
+++ build.js
@@ -0,0 +1,18 @@
+const { promisify } = require('util')
+const fs = require('fs')
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
+readFile('./style.dev.json')
+  .then(data => JSON.parse(data))
+  .then(style =>
+    JSON.stringify(
+      {
+        ...style,
+        sprite: 'https://geolonia.github.io/geolonia-ar-field/grass'
+      },
+      null,
+      2
+    )
+  )
+  .then(data => writeFile('./style.json', data))
